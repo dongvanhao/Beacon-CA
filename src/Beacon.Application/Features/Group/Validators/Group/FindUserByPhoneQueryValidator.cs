1dba530e3fe842c4e965db9503cfbc0ca2b3935f
DongHao <dongvanhao12345@gmail.com>
2026-05-05 00:05:57 +0700

    fix(api/search): clean validate

--- src/Beacon.Application/Features/Group/Validators/Group/FindUserByPhoneQueryValidator.cs
+++ src/Beacon.Application/Features/Group/Validators/Group/FindUserByPhoneQueryValidator.cs
@@ -9,7 +9,6 @@ public class FindUserByPhoneQueryValidator : AbstractValidator<FindUserByPhoneQu
     {
         RuleFor(x => x.Search)
             .NotEmpty().WithMessage("Từ khoá tìm kiếm không được để trống.")
-            .MinimumLength(3).WithMessage("Từ khoá tìm kiếm phải có ít nhất 3 ký tự.")
             .MaximumLength(100).WithMessage("Từ khoá tìm kiếm không được vượt quá 100 ký tự.");
     }
 }
